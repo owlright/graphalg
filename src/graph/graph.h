dd826674e1cad520db1b7e1fdd79641e0d0ef072
jhsheng <jhsheng@mail.ustc.edu.cn>
2023-11-25 22:14:16 +0800

    feat(RuntimeError): generated by chatgpt

--- src/graph/graph.h
+++ src/graph/graph.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <algorithm>
+#include <cstdarg>
 #include <graphviz/gvc.h>
 #include <list>
 #include <map>
@@ -91,4 +92,48 @@ public:
     // Move assignment operator
     // auto a = getNetworkCopy() will call this
     Graph& operator=(Graph&& other);
+};
+
+class cRuntimeError : public std::runtime_error {
+public:
+    explicit cRuntimeError(const char* format, ...) noexcept
+        : std::runtime_error("")
+        , message(nullptr)
+    {
+        va_list args;
+        va_start(args, format);
+        formatMessage(format, args);
+        va_end(args);
+    }
+
+    ~cRuntimeError() noexcept override { delete[] message; }
+
+    const char* what() const noexcept override { return message ? message : std::runtime_error::what(); }
+
+private:
+    char* message;
+
+    void formatMessage(const char* format, va_list args)
+    {
+        const int bufferSize = 256;
+        char buffer[bufferSize];
+
+        // Try to format the message into a buffer
+        int n = vsnprintf(buffer, bufferSize, format, args);
+
+        if (n < 0) {
+            // vsnprintf failure, set an error message
+            message = new char[bufferSize];
+            snprintf(message, bufferSize, "Error formatting exception message");
+        } else if (n < bufferSize) {
+            // Message fits in the buffer
+            message = new char[n + 1];
+            strncpy(message, buffer, n);
+            message[n] = '\0';
+        } else {
+            // Message does not fit in the buffer, dynamically allocate memory
+            message = new char[n + 1];
+            vsnprintf(message, n + 1, format, args);
+        }
+    }
 };
