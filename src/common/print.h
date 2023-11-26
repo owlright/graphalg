2921d2575472aa383e0c67f5c36a57238a7a139f
jhsheng <jhsheng@mail.ustc.edu.cn>
2023-11-26 21:01:37 +0800

    separate << overload

---
+++ src/common/print.h
@@ -0,0 +1,92 @@
+
+#pragma once
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <unordered_set>
+#include <set>
+#include <ctime>
+#include <iomanip>
+#include <cstring>
+
+template <class T> std::ostream& operator<<(std::ostream& os, const std::vector<std::vector<T>>& matrix)
+{
+    for (auto i = 0; i < matrix.size(); i++) {
+        const auto& row = matrix[i];
+        for (const auto& element : row) {
+            os << element << " ";
+        }
+        if (i != matrix.size() - 1)
+            os << std::endl;
+    }
+    return os;
+}
+
+template <class T> std::ostream& operator<<(std::ostream& os, const std::vector<T>& array)
+{
+    os << "[";
+    for (auto i = 0; i < array.size(); i++) {
+        if (i != 0)
+            os << ",";
+        os << array[i];
+    }
+    os << "]";
+    return os;
+}
+
+template <typename K, typename V> std::ostream& operator<<(std::ostream& os, const std::unordered_map<K, V>& map)
+{
+    for (const auto& [key, value] : map) {
+        os << key << ": " << value << ", ";
+    }
+    return os;
+}
+
+template <typename K, typename V> std::ostream& operator<<(std::ostream& os, const std::map<K, V>& map)
+{
+    for (const auto& [key, value] : map) {
+        os << key << ": " << value << ", ";
+    }
+    return os;
+}
+
+template <class T> std::ostream& operator<<(std::ostream& os, const std::unordered_set<T>& array)
+{
+    os << "[";
+    int count = 0;
+    for (const auto& i : array) {
+        if (count != 0)
+            os << ",";
+        os << i;
+        count++;
+    }
+    os << "]";
+    return os;
+}
+
+template <class T> std::ostream& operator<<(std::ostream& os, const std::set<T>& array)
+{
+    os << "[";
+    int count = 0;
+    for (const auto& i : array) {
+        if (count != 0)
+            os << ",";
+        os << i;
+        count++;
+    }
+    os << "]";
+    return os;
+}
+
+inline void printNiceTime(time_t& now)
+{
+    std::tm* localTime = std::localtime(&now);
+    int hours = localTime->tm_hour;
+    int minutes = localTime->tm_min;
+    int seconds = localTime->tm_sec;
+    char buffer[10];
+    memset(buffer, 0, sizeof(buffer));
+    sprintf(buffer, "%d:%d:%d", hours, minutes, seconds);
+    std::cout << std::left << std::setw(11) << buffer << std::endl;
+}
--- src/common/utils.h
+++ src/common/utils.h
@@ -2,16 +2,8 @@
 #include <cstdarg>
 #include <cstdlib>
 #include <cstring>
-#include <iostream>
-#include <list>
-#include <map>
-#include <set>
 #include <stdexcept>
-#include <unordered_map>
-#include <unordered_set>
 #include <vector>
-#include <ctime>
-#include <iomanip>
 
 #ifdef DEBUG
 #define ASSERT(expr)                                                                                                   \
@@ -27,17 +19,10 @@
     } while (false)
 #endif
 
-using std::cout;
-using std::endl;
-using std::list;
-using std::make_pair;
-using std::map;
-using std::pair;
-using std::runtime_error;
-using std::unordered_set;
-using std::vector;
-
 namespace graphalg {
+
+using std::runtime_error;
+using std::vector;
 
 class cRuntimeError : public runtime_error {
 public:
@@ -82,85 +67,4 @@ private:
         }
     }
 };
-}
-
-template <class T> std::ostream& operator<<(std::ostream& os, const std::vector<std::vector<T>>& matrix)
-{
-    for (auto i = 0; i < matrix.size(); i++) {
-        const auto& row = matrix[i];
-        for (const auto& element : row) {
-            os << element << " ";
-        }
-        if (i != matrix.size() - 1)
-            os << std::endl;
-    }
-    return os;
-}
-
-template <class T> std::ostream& operator<<(std::ostream& os, const std::vector<T>& array)
-{
-    os << "[";
-    for (auto i = 0; i < array.size(); i++) {
-        if (i != 0)
-            os << ",";
-        os << array[i];
-    }
-    os << "]";
-    return os;
-}
-
-template <typename K, typename V> std::ostream& operator<<(std::ostream& os, const std::unordered_map<K, V>& map)
-{
-    for (const auto& [key, value] : map) {
-        os << key << ": " << value << ", ";
-    }
-    return os;
-}
-
-template <typename K, typename V> std::ostream& operator<<(std::ostream& os, const std::map<K, V>& map)
-{
-    for (const auto& [key, value] : map) {
-        os << key << ": " << value << ", ";
-    }
-    return os;
-}
-
-template <class T> std::ostream& operator<<(std::ostream& os, const std::unordered_set<T>& array)
-{
-    os << "[";
-    int count = 0;
-    for (const auto& i : array) {
-        if (count != 0)
-            os << ",";
-        os << i;
-        count++;
-    }
-    os << "]";
-    return os;
-}
-
-template <class T> std::ostream& operator<<(std::ostream& os, const std::set<T>& array)
-{
-    os << "[";
-    int count = 0;
-    for (const auto& i : array) {
-        if (count != 0)
-            os << ",";
-        os << i;
-        count++;
-    }
-    os << "]";
-    return os;
-}
-
-inline void printNiceTime(time_t& now)
-{
-    std::tm* localTime = std::localtime(&now);
-    int hours = localTime->tm_hour;
-    int minutes = localTime->tm_min;
-    int seconds = localTime->tm_sec;
-    char buffer[10];
-    memset(buffer, 0, sizeof(buffer));
-    sprintf(buffer, "%d:%d:%d", hours, minutes, seconds);
-    cout << std::left << std::setw(11) << buffer << endl;
 }
